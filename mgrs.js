e246a9fe0a87e0c441610e57612fa1da89d83e82
DanielJDufour <daniel.j.dufour@gmail.com>
2019-05-06 19:03:40 +0000

    fixed eslint issue

--- mgrs.js
+++ mgrs.js
@@ -87,7 +87,7 @@ export function inverse(mgrs) {
 
 export function toPoint(mgrs) {
   if (mgrs === '') {
-    throw new TypeError(`toPoint received a blank string`);
+    throw new TypeError('toPoint received a blank string');
   }
   const bbox = UTMtoLL(decode(mgrs.toUpperCase()));
   if (bbox.lat && bbox.lon) {
