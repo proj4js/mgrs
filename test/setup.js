3654de62d988cebaff4b1617d03b490669819617
DanielJDufour <daniel.j.dufour@gmail.com>
2019-07-11 15:06:12 +0000

    fixed linting issues in test/setup.js

--- test/setup.js
+++ test/setup.js
@@ -8,29 +8,30 @@ async function process() {
   // geotrans3.7/SpreadsheetTester/TestFiles/outputs/output/mgrsToGeo_WE.txt
   const url = 'https://s3.amazonaws.com/mgrs.io/mgrsToGeo_WE.txt';
 
-  let text = await fetch(url).then(response => response.text());
+  const text = await fetch(url).then(response => response.text());
 
+  // eslint-disable-next-line no-unused-vars
   const [header, description, blank, ...rows] = text.split('\r\n');
 
-  let testCases = rows
-  .filter(testCase => {
-    return testCase.includes('Successful-Equivalent');
-  })
-  .map(row => {
-    const cells = row.replace(/\t+/g, '\t').split('\t');
-    return {
-      latitude: cells[6].trim(),
-      longitude: cells[7].trim(),
-      mgrs: cells[5].trim()
-    };
-  })
+  const testCases = rows
+    .filter(testCase => {
+      return testCase.includes('Successful-Equivalent');
+    })
+    .map(row => {
+      const cells = row.replace(/\t+/g, '\t').split('\t');
+      return {
+        latitude: cells[6].trim(),
+        longitude: cells[7].trim(),
+        mgrs: cells[5].trim()
+      };
+    });
 
 
-  console.log("testCases", testCases);
-  const csv = testCases.map(({mgrs, latitude, longitude}) => `${mgrs}\t${latitude}\t${longitude}`).join('\n');
+  console.log('testCases', testCases);
+  const csv = testCases.map(({ mgrs, latitude, longitude }) => `${mgrs}\t${latitude}\t${longitude}`).join('\n');
   writeFileSync('testing-data.csv', csv, 'utf8');
 
-  console.log("wrote testing-data.csv");
+  console.log('wrote testing-data.csv');
 }
 
 process();
